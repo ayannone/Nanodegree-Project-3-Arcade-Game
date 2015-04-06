a4df0374e808d35dea2c1d98259c3dd36abbda0f
atrapp <andrea.trapp@gmx.net>
2015-04-05 23:16:51 -0400

    added styling with header, aside, main div ... etc

--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,8 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    var mainDiv = document.getElementById('main');
+    mainDiv.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
